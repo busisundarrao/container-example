d3a0c5aee6c9c32126b9ce2c5e66dd0d4411c638
busisundarrao <138547537+busisundarrao@users.noreply.github.com>
2023-08-29 11:03:34 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello{  
 public static void main(String[] args){  
 System.out.println("This is java app \n by using Docker");
-System.out.println("creating using docker file");  
+System.out.println("creating using docker file"); 
+System.out.println("creating image");  
 }  
 }  
